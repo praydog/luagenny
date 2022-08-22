3b2883f99d22795ce0311303788c9b05081ac46b
praydog <praydog@praydog.com>
2022-08-21 23:02:03 -0700

    Add more tests

--- test/test.lua
+++ test/test.lua
@@ -1,3 +1,5 @@
+local test_start = os.clock()
+
 baz = sdkgenny.StructOverlay.new(bazaddr, parsed:global_ns():find("struct", "Baz"))
 print(baz)
 print(baz:type())
@@ -44,7 +46,7 @@ local function value_expect(x, expected, metadata)
     end
 
     if x ~= expected then
-        print("test " .. tostring(test_count) .. " failed, got " .. tostring(x) .. ", expected " .. tostring(expected))
+        print("test " .. tostring(test_count) .. " failed " .. "[ " .. metadata .. " ]" .. ", got " .. tostring(x) .. ", expected " .. tostring(expected))
         return false
     end
 
@@ -55,16 +57,45 @@ end
 function do_tests()
     print("testing...")
 
+    local round = function(val, dec)
+        dec = dec or 1
+        return math.floor(val * 10 ^ dec + 0.5) / 10 ^ dec
+    end
+
     local results = {
         value_expect(baz.g.a, 43, "baz.g.a"),
         value_expect(baz.g.b, 1338, "baz.g.b"),
-        value_expect(math.ceil(baz.g.c), math.ceil(78.699996948242), "math.ceil(baz.g.c)"),
+        value_expect(round(baz.g.c, 1), 78.7, "round(baz.g.c)"),
         value_expect(baz.hello, "Hello, world!", "baz.hello"),
-        value_expect(baz.e, 666, "baz.e")
+        value_expect(baz.e, 666, "baz.e"),
+        value_expect(round(baz.ta.gpa, 1), 3.9, "baz.ta.gpa"),
+        value_expect(baz.ta.wage, 30000, "baz.ta.wage"),
+        value_expect(baz.ta.hours, 40, "baz.ta.hours"),
+        value_expect(bazstruct:find("variable", "e") ~= nil, true, "bazstruct:find(\"variable\", \"e\") ~= nil"),
+        value_expect(bazstruct:find("variable", "e"):is_a("variable"), true, "bazstruct:find(\"variable\", \"e\"):is_a(\"variable\")"),
+        value_expect(bazstruct:find("variable", "e"):is_a("class"), false, "bazstruct:find(\"variable\", \"e\"):is_a(\"type\")"),
+        value_expect(bazstruct:find("variable", "e"):is_a("struct"), false, "bazstruct:find(\"variable\", \"e\"):is_a(\"type\")"),
+        value_expect(bazstruct:find("variable", "e"):is_a("type"), false, "bazstruct:find(\"variable\", \"e\"):is_a(\"type\")"),
+        value_expect(bazstruct:find("variable", "not_real_var") == nil, true, "bazstruct:find(\"variable\", \"not_real_var\") == nil"),
+        
+        value_expect(baz.things:type():is_a("pointer"), true, "baz.things:type():is_a(\"pointer\")"),
+        value_expect(baz.things:type():to():is_a("struct"), true, "baz.things:type():to():is_a(\"struct\")"),
+        value_expect(baz.things:type():name(), "Thing*", "baz.things:type():name()"),
     }
 
     for i=0, 10-1 do
         table.insert(results, value_expect(baz.things[i].abc, i * 2, "baz.things[" .. tostring(i) .. "].abc"))
+    end
+
+    local known_variables = bazstruct:get_all("variable")
+
+    table.insert(results, value_expect(known_variables ~= nil, true, "known_variables ~= nil"))
+    table.insert(results, value_expect(#known_variables, 10, "#known_variables"))
+
+    for k, v in pairs(known_variables) do
+        local mt = getmetatable(baz)
+        local ok, val = pcall(mt.__index, baz, v:name())
+        table.insert(results, value_expect(ok, true, "pcall baz." .. v:name()))
     end
 
     for k, v in pairs(results) do
@@ -77,6 +108,16 @@ function do_tests()
     return true
 end
 
+local retval = do_tests()
+
+local time_elapsed = os.clock() - test_start
+
+print("Tests took " .. tostring(time_elapsed) .. " seconds (" .. tostring(time_elapsed * 1000) .. "ms)")
+
+local gen_start = os.clock()
 parsed:generate("test/")
 
-return do_tests()
+local gen_time = os.clock() - gen_start
+print("Generation took " .. tostring(gen_time) .. " seconds (" .. tostring(gen_time * 1000) .. "ms)")
+
+return retval
